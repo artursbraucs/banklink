0f13cb3c32c061d51fd4e013c105e6c2f1cfdb64
Arturs Braucs <arturs.braucs@gmail.com>
2014-02-18 08:33:42 +0200

    Remove rails load pathes

--- lib/banklink.rb
+++ lib/banklink.rb
@@ -18,10 +18,3 @@ require 'banklink/base'
 require 'banklink/helper'
 require 'banklink/swedbank'
 require 'banklink/notification'
-
-%w{ models controllers helpers }.each do |dir|
-  path = File.join(File.dirname(__FILE__), 'app', dir)
-  $LOAD_PATH << path
-  # ActiveSupport::Dependencies.load_paths << path
-  # ActiveSupport::Dependencies.load_once_paths.delete(path)
-end
